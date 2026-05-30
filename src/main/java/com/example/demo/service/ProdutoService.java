cc7b1d48c84806091490d29354af6cded9979615
EduardoMeM <moulinmolinae@gmail.com>
2026-05-29 23:18:59 -0300

    Criando  a funçao BUSCAR e DELETE

--- src/main/java/com/example/demo/service/ProdutoService.java
+++ src/main/java/com/example/demo/service/ProdutoService.java
@@ -1,14 +1,20 @@
 package com.example.demo.service;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.example.demo.entity.Produto;
 import com.example.demo.repository.ProdutoRepository;
 
 @Service
 public class ProdutoService {
     @Autowired
-    private ProdutoRepository produtoRepository;
+    private final ProdutoRepository produtoRepository;
+
+    public ProdutoService(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
 
     public Produto salvarProduto(Produto produto) {
         return produtoRepository.save(produto);
