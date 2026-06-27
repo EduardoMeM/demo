a282ed6971b523daa57c38cb1f9a50ae63eb5618
EduardoMeM <moulinmolinae@gmail.com>
2026-06-26 21:07:11 -0300

    inicializando com JavaScript

---
+++ src/main/java/com/example/demo/frontwend/script._cliente.js
@@ -0,0 +1,11 @@
+let campoNome = document.getElementById("nome").value;
+let campoEmail = document.getElementById("email").value;
+let campoTelefone = document.getElementById("telefone").value;
+function cadastrarCliente() {
+    let campoNome = document.getElementById("nome").value;
+    let campoEmail = document.getElementById("email").value;
+    let campoTelefone = document.getElementById("telefone").value;
+    alert(campoNome);
+    alert(campoEmail);
+    alert(campoTelefone);
+}
---
+++ src/main/java/com/example/demo/frontwend/script._produto.js
@@ -0,0 +1,11 @@
+let campoNomeproduto = document.getElementById("nomeproduto").value;
+let campoPreco = document.getElementById("preco").value;
+let campoQuantidade = document.getElementById("quantidade").value;
+function cadastrarProduto() {
+    let campoNomeproduto = document.getElementById("nomeproduto").value;
+    let campoPreco = document.getElementById("preco").value;
+    let campoQuantidade = document.getElementById("quantidade").value;
+    alert(campoNomeproduto);
+    alert(campoPreco);
+    alert(campoQuantidade);
+}
