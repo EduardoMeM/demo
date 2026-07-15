d9b206467e59c7533b0334e357ed50658e96398d
EduardoMeM <moulinmolinae@gmail.com>
2026-07-14 21:33:26 -0300

    finalizando as funcoes que faltava(buscar, deletar,atualizar)

--- src/main/java/com/example/demo/frontwend/script._cliente.js
+++ src/main/java/com/example/demo/frontwend/script._cliente.js
@@ -1,4 +1,5 @@
 function cadastrarCliente() {
+    let campoId=document.getElementById("id").value;
     let campoNome = document.getElementById("nome").value;
     let campoEmail = document.getElementById("email").value;
     let campoTelefone = document.getElementById("telefone").value;
@@ -38,6 +39,10 @@ function listar_cliente(){
                     <td>${cliente.nome_cliente}</td>
                     <td>${cliente.email_cliente}</td>
                     <td>${cliente.telefone_cliente}</td>
+                    <td>
+                    <button style="background-color: salmon; color:black" onclick="deletar_cliente(${cliente.id_cliente})">Deletar</button>
+                    <button style="background-color: lightblue; color:black" onclick="atualizar_cliente(${cliente.id_cliente})">Atualizar</button>
+                    </td>
                 </tr>`;
             });
         })
@@ -46,6 +51,67 @@ function listar_cliente(){
                 alert(error);
             });
 }
+
+function buscar_cliente(){
+    let campoId = document.getElementById("id").value;
+    console.log("Chamando API...");
+    fetch(`http://localhost:3333/clientes/${campoId}`)
+    .then(response => response.json())
+    .then(cliente => {
+        let tabela = document.getElementById("tabelaClientes");
+        tabela.innerHTML = "";
+        tabela.innerHTML += `<tr>
+            <td>${cliente.id_cliente}</td>
+            <td>${cliente.nome_cliente}</td>
+            <td>${cliente.email_cliente}</td>
+            <td>${cliente.telefone_cliente}</td>
+        </tr>`;
+    }).catch(error=>{
+        console.error(error);
+        alert("Cliente não encontrado.")
+    });
+}
+function deletar_cliente(id_cliente){
+    if(!confirm("Tem certeza que deseja deletar o cliente?")){
+        return;
+    }
+    fetch(`http://localhost:3333/clientes/${id_cliente}`, {
+        method: 'DELETE'
+    }).then(response => {
+        if(response.ok){
+            alert("Cliente deletado com sucesso!");
+            listar_cliente();
+        }else{
+            alert("Error ao deletar cliente.");
+        }
+    }).catch(error => {
+        console.error(error);
+        alert("Error ao deletar cliente.");
+    });}
+
+function atualizar_cliente(id_cliente){
+    let campoNome = document.getElementById("nome").value;
+    let campoEmail = document.getElementById("email").value;
+    let campoTelefone = document.getElementById("telefone").value;
+    const cliente = {
+        nome_cliente: campoNome,
+        email_cliente: campoEmail,
+        telefone_cliente: campoTelefone};
+    fetch(`http://localhost:3333/clientes/${id_cliente}`, {
+        method: 'PUT',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(cliente)
+    }).then(response => {
+        if(response.ok){
+            alert("Cliente atualizado com sucesso!");
+            listar_cliente();
+            cadastroLimpar();
+        }else{
+            alert("Erro ao atualizar cliente.");
+        }}).catch(error => {
+        console.error(error);
+        alert("Erro ao atualizar cliente.");
+    });}
           
 function cadastroLimpar(){
     let campoNome = document.getElementById("nome").value="";
